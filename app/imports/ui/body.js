d393b48166f0ebfe1338f8feae0819c86b4057ff
pokerplayer123 <z969aaron969z@gmail.com>
2017-08-30 23:20:23 +1000

    login page ui and account creation test (can't seem to get logout to work at the moment)

---
+++ app/imports/ui/body.js
@@ -0,0 +1,14 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+import './body.html';
+
+Template.body.helpers({
+    function(){
+
+    }
+});
+
+Template.body.events({
+
+});
