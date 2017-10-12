eb5328501a1819545190d2a5cbe2f2c9c5f96481
rushmaa <arushma07@gmail.com>
2017-10-12 23:08:03 +1100

    finish rating answers functionalities

--- app/server/main.js
+++ app/server/main.js
@@ -2,4 +2,12 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  Meteor.users.allow({
+    update: function (userId, doc, fieldNames, modifier) {
+      //similar checks like insert
+      return true;
+    },
+  });
 });
+
